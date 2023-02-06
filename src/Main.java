99af8b61bd9738682613ef05d71a3bafbd113b50
user <emilbek.ergeshali@gmail.com>
2023-02-06 20:53:41 +0300

    com

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,11 +9,15 @@ public class Main {
 
     }
     public static void kvad(double a){
-        if (Math.pow(a,2)==0){
+
+       a = Math.sqrt(a);
+
+
+        if (a == (int) a){
             System.out.println(a);
         }
         else{
-            System.out.println("jdshfk&");
+           throw new Error();
         }
     }
 }
