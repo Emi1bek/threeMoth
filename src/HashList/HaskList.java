c0e1492cdc7310822bea41b98ec081c3829a7922
user <emilbek.ergeshali@gmail.com>
2023-02-17 19:54:08 +0300

    Создано новое файл

--- src/HashList/HaskList.java
+++ src/HashList/HaskList.java
@@ -5,32 +5,22 @@ import java.util.*;
 public class HaskList {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
-        Random rd = new Random(400);
+        Random rd = new Random();
         int sum = 0;
         for (int i = 0; i < 400; i++) {
             int varInt = rd.nextInt(0, 20);
             arrayList.add(varInt);
-
         }
-        //  System.out.print(arrayList+" ");
-        for (Integer ar : arrayList
-        ) {
+        for (Integer ar : arrayList) {
             sum += ar;
-
         }
         System.out.println(sum);
-        LinkedHashSet<Integer> hashSet= new LinkedHashSet<>();
-       hashSet.addAll(arrayList);
-        for (Integer ar : hashSet
-        ) {
+        LinkedHashSet<Integer> hashSet = new LinkedHashSet<>();
+        hashSet.addAll(arrayList);
+        for (Integer ar : hashSet) {
             sum += ar;
-
         }
         System.out.println(sum);
-
-        //HaskList<Integer> hashList =new HaskList<>();
-
-
     }
 
 }
