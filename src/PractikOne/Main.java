605cef7382257270bb9b58bf5bd66bab7ed3b668
user <emilbek.ergeshali@gmail.com>
2023-02-21 21:02:46 +0300

    Nfgishvf

--- src/PractikOne/Main.java
+++ src/PractikOne/Main.java
@@ -8,16 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         Map<User,BankAccount> user = new HashMap<>();
-        user.put(new User("Name1","Surname1"),new BankAccount(10000));
-        user.put(new User("Name2","Surname2"),new BankAccount(10000));
-        user.put(new User("Name3","Surname3"),new BankAccount(10000));
-        user.put(new User("Name4","Surname4"),new BankAccount(10000));
-        user.put(new User("Name5","Surname5"),new BankAccount(10000));
-        user.put(new User("Name6","Surname6"),new BankAccount(10000));
-        user.put(new User("Name7","Surname7"),new BankAccount(10000));
-        user.put(new User("Name8","Surname8"),new BankAccount(10000));
-        user.put(new User("Name9","Surname9"),new BankAccount(10000));
-        user.put(new User("Name10","Surname10"),new BankAccount(10000));
 
 
         for (Map.Entry<User,BankAccount> m : user.entrySet()) {
@@ -27,7 +17,7 @@ public class Main {
 
         System.out.println(user.keySet());
 
-   transaction(user.get("Name1"), ,1000);
+   //transaction((user.get("Name1"),BankAccount), new User("Name22","surname122") ,1000);
 
     }
 
---
+++ src/PractikOne/MainOne.java
@@ -0,0 +1,43 @@
+package PractikOne;
+
+import java.util.*;
+
+public class MainOne {
+
+    public static void main(String[] args) {
+
+        Random rd = new Random();
+        int[] array = new int[10];
+        int count = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rd.nextInt(1,10);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < i; j++) {
+
+                if (array[i] == array[j]){
+                    count++;
+                    map.put(array[i],count);
+                    count = 0;
+                }
+
+            }
+
+        }
+        System.out.println(Arrays.toString(array));
+
+        for (Map.Entry<Integer,Integer> m: map.entrySet()
+             ) {
+            System.out.println(m);
+
+        }
+      //  System.out.println(map);
+
+
+
+    }
+}
